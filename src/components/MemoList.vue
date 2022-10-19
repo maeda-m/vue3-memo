79314b6401c935041c7420daeb6a2643aa301e3f
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2022-10-20 00:49:26 +0900

    メモの編集開始の動作を実装した

--- src/components/MemoList.vue
+++ src/components/MemoList.vue
@@ -1,18 +1,41 @@
 <template>
   <i-list-group>
     <template v-for="(memo, id) in memos" v-bind:key="id">
-      <i-list-group-item v-on:click="emits('actionEdit', { id })">
-        <i-button>{{ memo.title }}</i-button>
+      <i-list-group-item
+        v-bind:active="activate === id"
+        v-on:click="actionEdit(id)"
+      >
+        <p block class="_text:left">
+          {{ memo.title }}
+        </p>
       </i-list-group-item>
     </template>
   </i-list-group>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 const emits = defineEmits(["actionEdit"]);
-const props = defineProps({
+defineProps({
   memos: {
     type: Object,
   },
 });
+
+const activate = ref("");
+const actionEdit = (id) => {
+  if (activate.value === id) {
+    // Skip
+  } else {
+    activate.value = id;
+    emits("actionEdit", { id });
+  }
+};
 </script>
+
+<style>
+.list-group-item:not(.-active) {
+  cursor: pointer;
+}
+</style>
